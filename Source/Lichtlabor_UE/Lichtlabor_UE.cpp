98b9e6a7c4c89db83507dad253c6d2b70c4cde71
unknown <nruesch@live.de>
2018-05-29 00:44:05 +0200

    added ue scene

---
+++ Source/Lichtlabor_UE/Lichtlabor_UE.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Lichtlabor_UE.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Lichtlabor_UE, "Lichtlabor_UE" );
---
+++ Source/Lichtlabor_UE/Lichtlabor_UEGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Lichtlabor_UE.h"
+#include "Lichtlabor_UEGameMode.h"
+
+
+
+

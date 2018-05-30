d61839a3f166be7131fdba639de948dd3a97c94d
RaoulBickmann <raoul.bickmann@googlemail.com>
2018-05-30 23:31:16 +0200

    New project, added lightmap

--- Source/Lichtlabor_UE/Lichtlabor_UE.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Lichtlabor_UE.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Lichtlabor_UE, "Lichtlabor_UE" );
--- Source/Lichtlabor_UE/Lichtlabor_UEGameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Lichtlabor_UE.h"
-#include "Lichtlabor_UEGameMode.h"
-
-
-
-
